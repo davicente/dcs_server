9c4bf5252190268d485b9c80f0b5ed8a670985ba
David Vicente <dvicente@full-on-net.com>
2020-10-07 19:51:09 +0200

    Unit tests for scheduling job method

--- test/metrics.js
+++ test/metrics.js
@@ -9,42 +9,42 @@ const metricsController = require("../src/controller/metrics/metrics");
 
 describe("Metrics Controller", () => {
     describe("Add and get metrics", () => {
-        it("Get not used metric", async () => {
+        it("Get not used metric", () => {
             const metricSum = metricsController.getMetricSum("NotUsed");
             assert.equal(metricSum, 0);
         });
 
-        it("Add one value to metric", async () => {
+        it("Add one value to metric", () => {
             const metricName = "oneValue";
             metricsController.addMetric(metricName, 7);
             const metricSum = metricsController.getMetricSum(metricName);
             assert.equal(metricSum, 7);
         });
 
-        it("Trying to add not a number", async () => {
+        it("Trying to add not a number", () => {
             const metricName = "notANumber";
             try {
                 metricsController.addMetric(metricName, "Not a number");
-            } catch(error) {
+            } catch (error) {
                 assert.equal(error, constants.NOT_NUMBER_ERROR);
             }
         });
 
-        it("Add float value to be rounded below to metric", async () => {
+        it("Add float value to be rounded below to metric", () => {
             const metricName = "floatValueBelow";
             metricsController.addMetric(metricName, 7.45);
             const metricSum = metricsController.getMetricSum(metricName);
             assert.equal(metricSum, 7);
         });
 
-        it("Add float value to be rounded above to metric", async () => {
+        it("Add float value to be rounded above to metric", () => {
             const metricName = "floatValueAbove";
             metricsController.addMetric(metricName, 7.51);
             const metricSum = metricsController.getMetricSum(metricName);
             assert.equal(metricSum, 8);
         });
 
-        it("Add value to be rounded to 0 to existing metric", async () => {
+        it("Add value to be rounded to 0 to existing metric", () => {
             const metricName = "existingMetric";
             metricsController.addMetric(metricName, 9);
             let metricSum = metricsController.getMetricSum(metricName);
@@ -54,7 +54,7 @@ describe("Metrics Controller", () => {
             assert.equal(metricSum, 9);
         });
 
-        it("Add several values to metric and sum value is incremented", async () => {
+        it("Add several values to metric and sum value is incremented", () => {
             const metricName = "severalValues";
             metricsController.addMetric(metricName, 7);
             let metricSum = metricsController.getMetricSum(metricName);
@@ -67,7 +67,7 @@ describe("Metrics Controller", () => {
             assert.equal(metricSum, 20);
         });
 
-        it("Add values to different metrics and sum values are incremented correctly", async () => {
+        it("Add values to different metrics and sum values are incremented correctly", () => {
             const metricName1 = "severalValues1";
             const metricName2 = "severalValues2";
             const metricName3 = "severalValues3";
@@ -89,6 +89,37 @@ describe("Metrics Controller", () => {
             metricsController.addMetric(metricName3, 0.9);
             metricSum = metricsController.getMetricSum(metricName3);
             assert.equal(metricSum, 11);
+        });
+    });
+
+    describe("Cleaning old metrics", () => {
+        it("Check metric sum after expired", done => {
+            process.env.SECONDS_TO_REMOVE_METRIC = 2;
+            const metricName = "MetricAfterExpired";
+            metricsController.addMetric(metricName, 7.8);
+            let metricSum = metricsController.getMetricSum(metricName);
+            assert.equal(metricSum, 8);
+            setTimeout(() => {
+                metricSum = metricsController.getMetricSum(metricName);
+                assert.equal(metricSum, 0);
+                done();
+            }, 2200);
+        });
+
+        it("Check metric sum after one out of two expired", done => {
+            process.env.SECONDS_TO_REMOVE_METRIC = 2;
+            const metricName = "TwoMetricsOneExpired";
+            metricsController.addMetric(metricName, 7.8);
+            setTimeout(() => {
+                metricsController.addMetric(metricName, 13.3);
+                let metricSum = metricsController.getMetricSum(metricName);
+                assert.equal(metricSum, 21);
+                setTimeout(() => {
+                    let metricSum = metricsController.getMetricSum(metricName);
+                    assert.equal(metricSum, 13);
+                    done();
+                }, 1000);
+            }, 1500);
         });
     });
 });
